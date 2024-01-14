273385e706dc6646df2dbbe1a6e3babbdbbce1d4
Andersen2407 <nicolai2407@gmail.com>
2024-01-14 14:52:02 +0100

    Updated M5

---
+++ Computerarkitektur_og_indlejrede_systemer/M5_ESP32/Exercise1_ExternalISR.ino
@@ -0,0 +1,95 @@
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+
+static const uint8_t msgQ_length = 10;
+static QueueHandle_t msgQ;
+
+SemaphoreHandle_t semaphore;
+
+
+void IRAM_ATTR buttonISR() {
+ xSemaphoreGiveFromISR(semaphore, NULL);  // Signal the task from the ISR
+}
+
+
+void incrementer(void *Params)
+{
+
+  int QItem;
+
+  int LocalCounter;
+
+  while (1)
+  {
+    if (xQueueReceive(msgQ, (void *)&QItem, 0) == pdTRUE)
+    { 
+      // Check if the value is 123
+      if( (int)QItem == 123)
+      {
+        LocalCounter++;
+        Serial.println(QItem);
+      }
+    }
+    else
+    {
+      Serial.println("No messages in queue!");
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  }
+
+}
+
+void ISRevent (void *pvParameters) 
+{
+  static int RandomInt;
+
+  while (1) 
+  {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) 
+    {
+      // Send random value to queue from 122 - 123.
+      RandomInt = random(122, 125);
+
+      if (xQueueSend(msgQ, (void *)&RandomInt, 10) != pdTRUE)
+      {
+        Serial.println("Queue is full!");
+      }
+    }
+  }
+
+}
+
+void setup() 
+{
+  // Setup baud
+  Serial.begin(115200);
+  pinMode(0, INPUT_PULLUP);
+  semaphore = xSemaphoreCreateBinary();
+  
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+
+  msgQ = xQueueCreate(msgQ_length, sizeof(int));
+  xTaskCreatePinnedToCore(incrementer, "incrementer", 1024, NULL, 1, NULL, app_cpu);
+
+  if (semaphore != NULL) 
+  {
+   // Attach the ISR to the button pin.
+   attachInterrupt(digitalPinToInterrupt(0), buttonISR, RISING);
+
+   // Create a task to handle the button press.
+   xTaskCreatePinnedToCore(ISRevent, "ISRdemo", 1024, NULL, 1, NULL, 1);
+  } 
+  else 
+  {
+   Serial.println("Semaphore creation failed!");
+  }
+}
+
+void loop() {}
