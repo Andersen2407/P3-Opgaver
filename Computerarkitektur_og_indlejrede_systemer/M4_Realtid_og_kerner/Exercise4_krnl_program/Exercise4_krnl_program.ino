df57e2baa99bc86d985926dcbb8948138397ec33
Mikkel-Coder <ms22@student.aau.dk>
2024-01-16 16:28:21 +0100

    Added exercise 4 for M4

--- Computerarkitektur_og_indlejrede_systemer/M4_Realtid_og_kerner/Exercise4_krnl_program/Exercise4_krnl_program.ino
+++ Computerarkitektur_og_indlejrede_systemer/M4_Realtid_og_kerner/Exercise4_krnl_program/Exercise4_krnl_program.ino
@@ -3,15 +3,14 @@
 const int STKSZ = 200;
 char stak1[STKSZ], stak2[STKSZ];
 
-struct k_t *pt1,  // pointer to hold reference
-  *pt2;           // to taskdescriptor for t1 and t2
+struct k_t *pt1, *pt2;  // pointer to hold reference to taskdescriptor for t1 and t2
 
 void t1(void) {
   while (1) {
-    DI();
-    k_sleep(100);
-    digitalWrite(13, !digitalRead(13));
-    EI();
+    DI();                       // Disable interrupts
+    k_sleep(100);               // Put task to sleep mode
+    digitalWrite(13, !digitalRead(13)); // Flip LED output
+    EI();                       // Enable interrupts
   }
 }
 
@@ -19,6 +18,8 @@ void t2() {
   unsigned int Counter = 0;
   unsigned long StartTime;
 
+  // The task counts the amount of prints and how long they took. Here k_sleep makes the task take 400 ticks on the cpu
+  // it reserves this due to non-preemptive scheduling
   while (1) {
     DI();
     StartTime = millis();
@@ -35,8 +36,13 @@ void setup() {
   Serial.begin(115200);
   pinMode(13, OUTPUT);
 
-  // init krnl so you can create 2 tasks, no semaphores and no message queues
+  // initialize kernel so you can create 2 tasks, 0 semaphores and 0 message queues
   k_init(2, 0, 0);
+
+                  // Method which task will use
+                      // Priority
+                           // Stack the task will use
+                                  // Stack size
   pt1 = k_crt_task(t1, 11, stak1, STKSZ);
   pt2 = k_crt_task(t2, 11, stak2, STKSZ);
 
