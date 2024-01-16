4019644bd0213557d70f2b29dad2839b347180c8
Mikkel-Coder <ms22@student.aau.dk>
2024-01-16 14:51:20 +0100

    Added exercise 2 for M3
    Co-authored-by: TheBear <thebear132@users.noreply.github.com>
    Co-authored-by: Andersen2407 <Andersen2407@users.noreply.github.com>
    Co-authored-by: simonsvale <simonsvale@users.noreply.github.com>

---
+++ Computerarkitektur_og_indlejrede_systemer/M3 Arduino_tid,_realtid,_sampling_og_interupts/Exercise2_AD_Conversion/Exercise2_AD_Conversion.ino
@@ -0,0 +1,36 @@
+// Get an AD conversion of the temperature by manual trigger e.g. the button press
+const int buttonPin = 2;
+
+void enable_temperature_conversion() {
+   // Set AD reference to 1.1V (internal) and enable reading the temperature sensor
+   // According to Datasheet "23.8 Temperature Measurement"
+   ADMUX = (1<<REFS1) | (1<<REFS0) | (1<<MUX3);
+   // ADMUX is the ADC (Analog to digital converter) Multiplexer Select (ADMUX).
+   // It selects a reference source (REFS pins - 1.1V internal reference)
+   // Also selects an analog source (via the MUX pins -> MUX3 -> Selects the interval temperature sensor)
+}
+
+void setup() {
+   Serial.begin(115200);
+   enable_temperature_conversion();
+
+   // Attach blink as interrupt whenever the buttonPin is RISING (trigger when the pin goes from low -> high)
+   attachInterrupt( digitalPinToInterrupt(buttonPin), blink, RISING);
+   pinMode(buttonPin, INPUT);
+}
+
+void blink(){
+   // enable fast conversion (spares 13 ticks and doesn't need to set the ADEN bit)
+   ADCSRA |= 1<<ADSC;
+
+   // wait for conversion to finish (ADSC will be set to 0 again when conversion is finished)
+   while (ADCSRA & (1<<ADSC)) {};
+
+   // Load the results 8 lower bits and 8 upper bits into a variable
+   uint16_t temperature = ADCL;
+   temperature += ADCH<<8;
+
+   Serial.println(temperature, DEC); // print it in base 10
+}
+
+void loop() {}
